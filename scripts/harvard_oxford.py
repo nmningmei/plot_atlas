7cb0f45087c3f625f8cd0564e4966f9a638a1fb2
NingM <semsociaty@gmail.com>
2020-04-26 15:33:18 +0800

    upload

--- scripts/harvard_oxford.py
+++ scripts/harvard_oxford.py
@@ -5,6 +5,56 @@ Created on Sun Apr 26 12:42:26 2020
 @author: ning
 """
 
-from nilearn import datasets
+import os
+from glob import glob
+from nilearn import datasets,plotting
 
-dataset = datasets.fetch_atlas_harvard_oxford()
+# create a directory for the atlas
+if not os.path.exists('../data'):
+    os.mkdir('../data')
+data_dir = os.path.join('../data','fmri_rois')
+
+#dataset = datasets.fetch_atlas_craddock_2012('../data')
+roi_names = """fusiform
+inferiorparietal
+inferiortemporal
+lateraloccipital
+lingual
+middlefrontal
+parahippocampal
+pericalcarine
+precuneus
+superiorfrontal
+superiorparietal
+parsorbitalis
+parstriangularis
+parsopercularis"""
+
+standard_brain = f'{data_dir}/MNI152_T1_2mm_brain.nii.gz'
+masks = glob(os.path.join(data_dir,'*standard*'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
