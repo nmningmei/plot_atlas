aeed561eb65b951af9e20e30067ba4d6fe37883d
NingM <semsociaty@gmail.com>
2020-04-26 17:14:08 +0800

    figure

--- scripts/plot_rois_on_standard_brain.py
+++ scripts/plot_rois_on_standard_brain.py
@@ -19,6 +19,9 @@ sns.set_context('poster')
 if not os.path.exists('../data'):
     os.mkdir('../data')
 data_dir = os.path.join('../data','fmri_rois')
+fig_dir = '../figures'
+if not os.path.exists(fig_dir):
+    os.mkdir(fig_dir)
 
 #dataset = datasets.fetch_atlas_craddock_2012('../data')
 roi_names = """fusiform
@@ -39,6 +42,7 @@ ventrolateralPFC""".split('\n')
 #parsopercularis
 
 the_brain = f'{data_dir}/MNI152_T1_2mm_brain.nii.gz'
+the_mask = f'{data_dir}/MNI152_T1_2mm_brain_mask_dil.nii.gz'
 # get the standard ROIs
 masks = glob(os.path.join(data_dir,'*standard*'))
 #masks = [item for item in glob(os.path.join(data_dir,'*.nii.gz')) if ('BOLD' not in item) and ('fsl' not in item) and ('standard' not in item) and ('MNI152' not in item)]
@@ -70,7 +74,7 @@ handles, labels = [],[]
 # I prefer to control it this way so that I will know the correspondence
 from matplotlib.colors import ListedColormap
 # get the colors into a numpy array
-color_list = plt.cm.tab20(np.arange(len(roi_names)))
+color_list = plt.cm.Paired(np.arange(len(roi_names)))
 # convert the numpy array of colors to a colormap object for plotting
 cmap = ListedColormap(color_list)
 
@@ -96,6 +100,9 @@ for ii,(color,mask_name) in enumerate(zip(color_list,roi_names)):
     handles.append(Patch(facecolor = color))
     labels.append(mask_name)
 
+# bound the mask
+mask_boundary = load_mri(the_mask).get_data()
+combined_mask[mask_boundary == 0] = 0
 # create a niftilImage from a numpy array
 combined_mask = image.new_img_like(mask[0],
                                    combined_mask,)
@@ -119,7 +126,9 @@ ax.legend(handles,
            loc = "center right",
            borderaxespad = 1,
            )
-
+fig.savefig(os.path.join(fig_dir,'ROIs.jpeg'),
+            dpi = 400,
+            bbox_inches = 'tight',)
 
 #left = [ '../data\\fmri_rois\\ctx-lh-parsopercularis.nii.gz',
 # '../data\\fmri_rois\\ctx-lh-parsorbitalis.nii.gz',
